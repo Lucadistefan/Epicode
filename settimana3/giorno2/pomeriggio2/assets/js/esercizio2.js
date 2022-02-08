c3ae5f2f862e89b8b7f098dc7553af4c5421bcdd
Luca <lucads94de@gmail.com>
2022-02-08 18:24:30 +0100

    javascript

--- settimana3/giorno2/pomeriggio2/assets/js/esercizio2.js
+++ settimana3/giorno2/pomeriggio2/assets/js/esercizio2.js
@@ -1,31 +1,31 @@
 var addBtn;
 var taskTxt;
 var taskListHTML;
-// dichiarando le mie variabili
-
+var tasksList = [];
+// dichiara le variabili
 window.addEventListener("load", init);
 // al caricamento della pagina chiama la funzione init
 function init() {
-  addBtn = document.querySelector("add_btn");
-  taskTxt = document.querySelector("task_txt");
-  taskListHTML = document.querySelector("#task_list_html");
+  addBtn = document.querySelector("#add_btn");
+  taskTxt = document.querySelector("#task_txt");
+  taskListHTML = document.querySelector("#tasks_list_html");
   // aggancia alle variabili gli oggetti html
   addBtn.addEventListener("click", addTask);
   // si mette in ascolto in attesa di un evento click sul bottone
-  // quando avviene il click chiamo addTask
+  // quando avviene il click chiama addtask
 }
 
 function addTask() {
   tasksList.push(taskTxt.value);
-  // inserisce nell'array tasklist il value scritto nell'input
-  // chiamo le altre due funzioni
+  // inserisce nell'array tasklist il valore scritto nell'input
+  // chiama le 2 funzioni sotto
   buildList();
   clearForms();
 }
 
 function buildList() {
   var list = "";
-  // pulisco
+  // pulisce la stringa
   for (var i = 0; i < tasksList.length; i++) {
     // fa il ciclo sull'array ottenuto sopra
     list += "<li>" + tasksList[i] + "</li>";
@@ -36,5 +36,5 @@ function buildList() {
 }
 
 function clearForms() {
-  taskTxt.value = ""; // ripulisco il campo input
+  taskTxt.value = ""; // pulisce al caricamento
 }
