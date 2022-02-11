8f4adac5fc69887ec45a17f0f54589a13debe4c1
Luca <lucads94de@gmail.com>
2022-02-11 17:26:12 +0100

    javascript

---
+++ settimana3/giorno5/assets/js/tabella.js
@@ -0,0 +1,37 @@
+var voti = [];
+var voto_medio;
+
+window.addEventListener("load", media);
+
+function media() {
+  let studentiV = document.getElementsByClassName("voto");
+  for (let i = 0; i < studentiV.length; i++) {
+    voti[i] = Number(studentiV[i].textContent);
+  }
+  for (var n = 0, tot = 0; n < voti.length; n++) {
+    tot = tot + voti[n];
+  }
+  voto_medio = tot / voti.length;
+  console.log(tot, voto_medio);
+  lettera();
+}
+
+function lettera() {
+  switch (true) {
+    case voto_medio < 60:
+      console.log("la lettera è E");
+      break;
+    case voto_medio < 70:
+      console.log("la lettera è D");
+      break;
+    case voto_medio < 80:
+      console.log("la lettera è C");
+      break;
+    case voto_medio < 90:
+      console.log("la lettera è B");
+      break;
+    default:
+      console.log("la lettera è A");
+      break;
+  }
+}
