e298c3219910ca17040836e6a116b4070f282da3
Luca <lucads94de@gmail.com>
2022-02-14 18:39:07 +0100

    jquery

--- settimana4/giorno1/pomeriggio1/assets/js/link.js
+++ settimana4/giorno1/pomeriggio1/assets/js/link.js
@@ -1,5 +1,13 @@
-$("body a:first-child").click(function (event) {
+$("body a:eq(0)").click(function (event) {
   event.preventDefault();
-  $("body div:eq(2)").toggle(1000);
-  $("body div:eq(2)").show();
+  $("body div:eq(0)").toggle(1000);
+  crea();
 });
+
+$("body a:eq(1)").click(function (a) {
+  a.preventDefault();
+  $("body div:eq(1)").toggle(1000);
+  $("body div:eq(0)").html('<a href="" class="box3">tasto 2</a>');
+});
+
+function crea() {}
