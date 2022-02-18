c6b8b874dfd9d9ebea104fc8bc69a065bdaa84a7
Luca <lucads94de@gmail.com>
2022-02-18 11:00:31 +0100

    jquery

--- settimana4/giorno4/progetto_settimana4/assets/js/MyPlayer.js
+++ settimana4/giorno4/progetto_settimana4/assets/js/MyPlayer.js
@@ -1,29 +1,51 @@
-$(document).ready(function () {
-  var titolo;
-  var canzone;
-  var titolo2;
-  var filmato;
-  $.ajax({
-    url: "assets/json/media.json",
-    contentType: "application/json",
-    dataType: "json",
-    success: function (data) {
-      $.each(data, function (i, el) {
-        var titolo = el.title;
-        console.log(titolo);
-        $("#canzoni").append(
-          '<ul class="list-group col-md-6"><li class="list-group-item">' +
-            "titolo" +
-            "</li></ul>"
-        );
-        var titolo2 = el.title;
-        console.log(titolo2);
-        $("#filmato").append(
-          '<ul class="list-group col-md-6"><li class="list-group-item">' +
-            "titolo2" +
-            "</li></ul>"
-        );
-      });
-    },
-  });
+$(document).ready(function() {
+    liste = {};
+    $.ajax({
+        url: "assets/json/media.json",
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data) {
+            liste = data;
+            $.each(data.audio, function(i, el) {
+                var titolo = el.title;
+                console.log(titolo);
+                $("#ul_audio").append(
+                    '<li class="list-group-item" onclick="cambio(' +
+                    0 +
+                    "," +
+                    i +
+                    ')">' +
+                    titolo +
+                    "</li>"
+                );
+            });
+            $.each(data.video, function(i, el) {
+                var titolo2 = el.title;
+                console.log(titolo2);
+                $("#ul_video").append(
+                    '<li class="list-group-item" onclick="cambio(' +
+                    1 +
+                    "," +
+                    i +
+                    ')">' +
+                    titolo2 +
+                    "</li>"
+                );
+            });
+        },
+    });
 });
+
+function cambio(av, index) {
+    audio = document.querySelector("audio");
+    video = document.querySelector("video");
+    if (av == 0) {
+        $("#myaudio").attr("src", "assets/audio/" + liste.audio[index].file);
+        audio.play();
+        video.pause();
+    } else {
+        $("#myvideo").attr("src", "assets/video/" + liste.video[index].file);
+        video.play();
+        audio.pause();
+    }
+}
